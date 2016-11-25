ab06afd7582a657d85538d019c72f293d04a417e
Robert Cuprik <robertcuprik@gmail.com>
2016-11-25 20:28:12 +0100

    Add LexicalAnalyzer mock

---
+++ src/LexicalAnalyzer.java
@@ -0,0 +1,18 @@
+import tokens.Terminal;
+
+import java.util.List;
+
+/**
+ * Created by Gamer on 11/25/2016.
+ */
+public class LexicalAnalyzer {
+
+    LexicalAnalyzer (List<Terminal> validTerminals) {
+
+    }
+
+    public List<Terminal> getTerminalsFromString(String input){
+        return null;
+    }
+
+}
